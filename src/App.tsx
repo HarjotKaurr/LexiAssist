7ca221ce2e718569005758b81d53344223027981
itsmeee <ironfistback@gmail.com>
2025-11-26 17:36:41 +0530

    Authentication and Info Form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { TestProvider } from "./context/TestContext";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
@@ -21,18 +21,16 @@ const App = () => (
       <TestProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/screening" element={<Screening />} />
-              <Route path="/results" element={<Results />} />
-              <Route path="/word-detective" element={<WordDetective />} />
-              <Route path="/storybook" element={<Storybook />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Layout>
-        </BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/screening" element={<Screening />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/word-detective" element={<WordDetective />} />
+            <Route path="/storybook" element={<Storybook />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
       </TestProvider>
     </TooltipProvider>
   </QueryClientProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import AppWrapper from './AppWrapper.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<AppWrapper />);
